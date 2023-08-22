e3b7b2603a19433109128a36d56c3b513dd1b4d6
Gerlielbecario <fernando.huaranca99@gmail.com>
2023-08-22 17:41:26 -0300

    pruebas con archivos

--- tiempos_forecast.py
+++ tiempos_forecast.py
@@ -8,12 +8,17 @@ import matplotlib.pyplot as plt
 import numpy as np
 import cartopy.crs as ccrs
 import cartopy.feature as cfeature
+import os
 
-##############
+#Archivo .grib que deseamos leer
+file = "apcp_sfc_2009102400_c00.grib2"
 
-path = 'apcp_sfc_2013031600_c00.grib2'
+#Carpeta donde se encuentra el archivo
+folder = '/home/fernando.huaranca/datos/DATOS_REFORECAST/apcp_sfc'
 
-#Open file
+#Ruta del archivo
+path = os.path.join(folder,file)
+print(path)
 ds = xr.open_dataset(path)
 
 #Tiempo inicial de nuestro pronostico
@@ -27,13 +32,13 @@ print('Tiempo inicial: ',ds.time.values.astype(str))
 
 ###Para ir sumando los step
 
-#print('Tiempo posterior',ds.time.values+np.timedelta64(ds.step[0].values))
+print('Tiempo posterior',ds.time.values+np.timedelta64(ds.step[6].values))
 
 
 #tengo pronosticos cada 3 hs y tengo 10 dias de informacion
 
 #tiempo_inicial = ds.time.values
 #print(ds.step[0].values.astype(str)
-for i in range(80):
-    print('Tiempo',str(i),ds.time.values+np.timedelta64(ds.step[i].values))
+#for i in range(80):
+ #   print('Tiempo',str(i),ds.time.values+np.timedelta64(ds.step[i].values))
 
